d725c77fe6cee6386d05b1ed07bbe2072f13fbc7
Vyacheslav Kovalenko <vjacheslavkovalenko@gmail.com>
2023-07-27 00:59:20 +0300

    This is first commit.

--- app/src/main/java/neco/lesson_1/MainActivity.kt
+++ app/src/main/java/neco/lesson_1/MainActivity.kt
@@ -2,10 +2,16 @@ package neco.lesson_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var tvText: TextView? = null
+    private var number: Byte = 5
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvText = findViewById(R.id.tvText)
+        tvText?.setText(number.toString())
     }
 }
